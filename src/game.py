eda5adc46870e728c278049051249923a4f239d2
SakshamJain2552 <s3923854@student.rmit.edu.au>
2023-07-29 16:52:01 +1000

    move and shoot

--- src/game.py
+++ src/game.py
@@ -3,6 +3,11 @@ import random
 import comms
 from object_types import ObjectTypes
 
+import random
+import comms
+from object_types import ObjectTypes
+from math import atan2, degrees, sqrt, pi
+
 
 class Game:
     """
@@ -16,6 +21,8 @@ class Game:
         is called and will be available to be used in `respond_to_turn` if needed.
     """
     def __init__(self):
+
+        self.last = None
         tank_id_message: dict = comms.read_message()
         self.tank_id = tank_id_message["message"]["your-tank-id"]
 
@@ -83,15 +90,142 @@ class Game:
 
         return True
 
+    # def respond_to_turn(self):
+    #     """
+    #     This is where you should write your bot code to process the data and respond to the game.
+    #     """
+
+    #     # Write your code here... For demonstration, this bot just shoots randomly every turn.
+
+    #     comms.post_message({
+    #         "shoot": 90,
+    #         "move":100,
+    #         "path":[100, 100]
+    #     })
+#####################################################################################################################################################################################################
+    def distance(self, x1, y1, x2, y2):
+        return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+
+    # Helper function to calculate the angle between two points in degrees
+    def angle_between_points(self, x1, y1, x2, y2):
+        # return 90 - (180 / 3.1415926535) * (3.1415926535 + 3.1415926535 - (3.1415926535 + atan2(y2 - y1, x2 - x1)))
+        return atan2(y2- y1, x2-x1) * 180/ pi
+
+    # Helper function to get the tank's state
+    def get_tank_state(self):
+        tank = self.objects.get(self.tank_id)
+        if tank:
+            return tank["position"][0], tank["position"][1], tank["hp"], tank["powerups"]
+        return None
+
+    # Helper function to get the closest powerup's state
+    def get_closest_powerup(self, tank_x, tank_y):
+        closest_powerup = None
+        closest_distance = float("inf")
+
+        for obj_id, obj_info in self.objects.items():
+            if obj_info["type"] == ObjectTypes.POWERUP.value:
+                powerup_x, powerup_y = obj_info["position"]
+                powerup_distance = self.distance(tank_x, tank_y, powerup_x, powerup_y)
+
+                if powerup_distance < closest_distance:
+                    closest_powerup = obj_info
+                    closest_distance = powerup_distance
+
+        return closest_powerup
+
+    # Helper function to check if a tank can shoot at a target
+    def can_shoot_target(self, tank_x, tank_y, target_x, target_y):
+        target_distance = self.distance(tank_x, tank_y, target_x, target_y)
+        if target_distance <= 200:  # Adjust this threshold based on the tank's shooting range
+            return True
+        return False
+    
+    # def move_away_from_boundary(self):
+
+    #     tank_state = self.objects[self.tank_id]
+    #     tank_x, tank_y = tank_state["position"]
+
+    #     nearest_boundary_distance = float("inf")
+    #     nearest_boundary_position = None
+
+    #     # Find the nearest boundary
+    #     for obj_id, obj_state in self.objects.items():
+    #         if obj_state["type"] == ObjectTypes.BOUNDARY.value:
+    #             boundary_x, boundary_y = obj_state["position"]
+    #             distance_to_boundary = self.distance(tank_x, tank_y, boundary_x, boundary_y)
+    #             if distance_to_boundary < nearest_boundary_distance:
+    #                 nearest_boundary_distance = distance_to_boundary
+    #                 nearest_boundary_position = (boundary_x, boundary_y)
+
+    #     if nearest_boundary_position is not None:
+    #         return (-boundary_x, -boundary_y)
+
+
+    # The bot's main decision-making function
     def respond_to_turn(self):
-        """
-        This is where you should write your bot code to process the data and respond to the game.
-        """
+        # Get the tank's state
+        tank_state = self.get_tank_state()
+        if not tank_state:
+            return
 
-        # Write your code here... For demonstration, this bot just shoots randomly every turn.
+        tank_x, tank_y, tank_hp, tank_powerups = tank_state
 
-        comms.post_message({
-            "shoot": random.uniform(0, random.randint(1, 360))
-        })
+        # bullets_to_avoid = []
+        # for obj_id, obj_state in self.objects.items():
+        #     if obj_state["type"] == ObjectTypes.BULLET.value:
+        #         bullet_x, bullet_y = obj_state["position"]
+        #         bullet_vx, bullet_vy = obj_state["velocity"]
+        #         bullet_distance = self.distance(bullet_x, bullet_y, tank_x, tank_y)
+        #         time_to_reach = bullet_distance / sqrt(bullet_vx ** 2 + bullet_vy ** 2)
+        #         if time_to_reach <= 1.5:  # Adjust this threshold based on the bullet speed
+        #             bullets_to_avoid.append((bullet_x, bullet_y))
 
+        # Calculate the angle towards the center of the map
+        center_x, center_y = self.width / 2, self.height / 2
+        angle_to_center = self.angle_between_points(tank_x, tank_y, center_x, center_y)
+
+        # Check if there are bullets on the way to the center
+        # for bullet_x, bullet_y in bullets_to_avoid:
+        #     angle_to_bullet = self.angle_between_points(tank_x, tank_y, bullet_x, bullet_y)
+        #     angle_difference = abs(angle_to_bullet - angle_to_center)
+        #     if angle_difference < 90:  # Adjust this angle threshold based on your strategy
+        #         # If the bullet is on the way, calculate a new angle to avoid it
+        #         angle_to_center += 90 if angle_to_bullet > angle_to_center else -90
+
+        # Get the opponent's state if available
+        opponent_state = None
+        for obj_id, obj_info in self.objects.items():
+            if obj_info["type"] == ObjectTypes.TANK.value and obj_id != self.tank_id:
+                opponent_state = obj_info["position"][0], obj_info["position"][1], obj_info["hp"], obj_info["powerups"]
+                break
+
+        # Get the closest powerup if available
+        closest_powerup = self.get_closest_powerup(tank_x, tank_y)
+        # bn = self.move_away_from_boundary()
+
+        # Decide the tank's action based on the situation
+        tank_action = None
+
+        # If an opponent is nearby, try to shoot at them
+        if opponent_state and self.can_shoot_target(tank_x, tank_y, opponent_state[0], opponent_state[1]):
+            angle_to_opponent = self.angle_between_points(tank_x, tank_y, opponent_state[0], opponent_state[1])
+            tank_action = {"shoot": angle_to_opponent}
+
+        # If no opponent nearby, move towards the closest powerup
+        elif closest_powerup:
+            powerup_x, powerup_y = closest_powerup["position"]
+            angle_to_powerup = self.angle_between_points(tank_x, tank_y, powerup_x, powerup_y)
+            tank_action = {"path": [powerup_x, powerup_y]}
+
+        # If there's nothing special, move randomly
+        elif self.last == None or self.last != [center_x, center_y]:
+            tank_action = {"path": [center_x, center_y]}
+            self.last = [center_x, center_y]
+
+        # Send the tank's action to the game server
+
+
+
+        comms.post_message(tank_action)
 
